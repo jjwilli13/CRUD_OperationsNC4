392faba4ec8a8962c47297ac219cb37464be867d
Jordan <jjwilli13@gmail.com>
2020-01-21 20:29:24 -0600

    final commit

---
+++ CRUD_OperationsNC4/ProductRepo.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace CRUD_OperationsNC4
+{
+    public class ProductRepo
+    {
+
+        public static string connString;
+
+        public List<Product> GetProducts()
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+            using (conn)
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "SELECT ProductID, Name, Price, StockLevel FROM products;";
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                var products = new List<Product>();
+                while (reader.Read())
+                {
+                    var row = new Product();
+                    row.ProductID = reader.GetInt32("ProductID");
+                    row.Name = reader.GetString("Name");
+                    row.Price = reader.GetDecimal("Price");
+                    row.StockLevel = reader.GetInt32("StockLevel");
+
+                    products.Add(row);
+                }
+                return products;
+
+            }
+
+
+        }
+
+        public void CreateProduct(Product p)
+        {
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO products(Name, Price, CategoryID, OnSale, StockLevel) " +
+                    "VALUES (@name, @price, @catID, @sale, @stock);";
+                cmd.Parameters.AddWithValue("name", p.Name);
+                cmd.Parameters.AddWithValue("price", p.Price);
+                cmd.Parameters.AddWithValue("catID", p.CategoryID);
+                cmd.Parameters.AddWithValue("sale", p.OnSale);
+                cmd.Parameters.AddWithValue("stock", p.StockLevel);
+
+                cmd.ExecuteNonQuery();
+
+            }
+
+
+        }
+
+        public void UpdateProduct(Product p)
+        {
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "UPDATE products SET StockLevel=@stock WHERE ProductID=@prodID;";
+                cmd.Parameters.AddWithValue("stock", p.StockLevel);
+                cmd.Parameters.AddWithValue("prodID", p.ProductID);
+
+                cmd.ExecuteNonQuery();
+
+            }
+
+        }
+
+        public void DeleteProduct(int id)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM products WHERE ProductID=@id;";
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        public void DeleteName(string name)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM products WHERE Name=@name;";
+                cmd.Parameters.AddWithValue("name", name);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+        public void DeleteIdName(int id, string name)
+        {
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "DELETE FROM products WHERE ProductID=@id AND Name=@name;";
+                cmd.Parameters.AddWithValue("name", name);
+                cmd.Parameters.AddWithValue("id", id);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
---
+++ CRUD_OperationsNC4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace CRUD_OperationsNC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string defaultKey = File.ReadAllText("appsettings.Debug.JSON");
+            JObject jObject = JObject.Parse(defaultKey);
+            JToken token = jObject["DefaultConnection"];
+            string connectionString = token.ToString();
+            ProductRepo.connString = connectionString;
+            
+
+
+
+            ProductRepo repo = new ProductRepo();
+
+            //Create Products
+            //Console.WriteLine("Creating Product......");
+            //var newProduct = new Product { Name = "Jack Hughes Product", Price = 19.99M, CategoryID = 2, OnSale = 0 };
+            //repo.CreateProduct(newProduct);
+            //Console.WriteLine("Product Created!");
+
+            //Update Product
+            //Console.WriteLine("Updating Product....");
+            //var newInfo = new Product { StockLevel = 27, ProductID = 944 };
+            //repo.UpdateProduct(newInfo);
+            //Console.WriteLine("Your Product has updated!");
+
+
+            //Delete Product
+            //Console.WriteLine("Deleting Product...");
+            //repo.DeleteIdName(946, "HAYS Product");
+            //repo.DeleteName("Jays Product");
+            //repo.DeleteProduct(947);
+
+
+
+            //Read Products
+            List<Product> products = repo.GetProducts();
+            foreach (var prod in products)
+            {
+                Console.WriteLine($"{prod.ProductID}  {prod.Name} -------- ${prod.Price}-----You have {prod.StockLevel} of these items.");
+
+            }
+
+
+
+        }
+    }
+}
